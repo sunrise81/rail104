a8eebefa8f76219f912c2bd46fd20899e7091882
sunrise <luoruijunblue@hotmail.com>
2017-01-01 16:16:29 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,37 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params[:id])
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update
+      redict_to groups_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
 end
