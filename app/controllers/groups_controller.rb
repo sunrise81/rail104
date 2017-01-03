72600c35b049d6531f82facb820311f83a3f5a8a
sunrise <luoruijunblue@hotmail.com>
2017-01-03 22:55:03 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -46,9 +46,9 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     if !current_user.is_member_of?(@group)
       current_user.join!(@group)
-      flash[:notice] = "加入本讨论组成功！"
+      flash[:notice] = "加入本讨论版成功！"
     else
-      flash[:warning] = "你已经是本讨论组成员了！"
+      flash[:warning] = "你已经是本讨论版成员了！"
     end
     redirect_to group_path(@group)
   end
@@ -60,8 +60,9 @@ class GroupsController < ApplicationController
       current_user.quit!(@group)
       flash[:alert] = "已退出本讨论版！"
     else
-      flash[:warning] = "你不是本讨论组成员，怎么退出 XD"
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
     end
+    redirect_to group_path(@group)
   end
 
   private
